b41b463022d084545d83a73c6918fa7bb267fab2
JulieKislaya <kislayajulie@gmail.com>
2022-07-24 19:30:53 +0300

    #4 implementation

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ Game 2048 Implementation
 3. After every interaction new table is drown
 4. User plays with keys a,s,d,w
 5. New blocks are generated with rate 2 - 90%, 4 - 10%
+6. Game ends when no possible motions
--- src/com/game/Game.java
+++ src/com/game/Game.java
@@ -1,11 +1,15 @@
 package com.game;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Game {
-
+   static List<Character>  list= List.of('a','s','w','d');
     public static void main(String[] args) throws IOException {
         char c= (char) System.in.read();
+       if(list.contains(c)) {
+           System.out.println("correct symbol, let's game");
+       }
     }
 }
